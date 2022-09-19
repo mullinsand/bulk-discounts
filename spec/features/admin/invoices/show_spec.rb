42f1d27f8ef399a9d55997b244f0d76ad044dc38
Carter Ball <carter.k.ball@gmail.com>
2022-09-19 15:50:10 -0400

    Refactor invoice info section in show spec to check each invoice

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -5,23 +5,51 @@ RSpec.describe 'The Admin Invoice Show' do
     @invoices = create_list(:invoice, 20)
     visit admin_invoice_path(@invoices[0])
   end
+  
+  describe 'invoice info section' do
+    it 'shows the invoice ID' do
+      within("#invoice_info") do
+        @invoices.each do |inv|
+          visit admin_invoice_path(inv)
+          expect(page).to have_content(inv.id)
+        end
+      end
+    end
 
-  it 'shows the invoice ID' do
-    expect(page).to have_content(@invoices[0].id)
-    expect(page).to_not have_content(@invoices[1].id)
+    it 'shows the invoice status' do
+      within("#invoice_info") do
+        @invoices.each do |inv|
+          visit admin_invoice_path(inv)
+          expect(page).to have_content(inv.status)
+        end
+      end
+    end
+
+    it 'shows the invoices created at date in the correct format' do
+      within("#invoice_info") do
+        @invoices.each do |inv|
+          visit admin_invoice_path(inv)
+          expect(page).to have_content(inv.created_at.strftime("%A, %B %d, %Y"))
+        end
+      end
+    end
+
+    it 'shows the invoice customers first and last name' do
+      within("#invoice_info") do
+        @invoices.each do |inv|
+          visit admin_invoice_path(inv)
+          expect(page).to have_content(inv.customer.first_name)
+          expect(page).to have_content(inv.customer.last_name)
+        end
+      end
+    end
   end
 
-  it 'shows the invoice status' do
-    expect(page).to have_content(@invoices[0].status)
-  end
+  describe 'invoice item information section' do
+    it 'shows all invoice item names on the invoice' do
+      
 
-  it 'shows the invoices created at date in the correct format' do
-    expect(page).to have_content(@invoices[0].created_at.strftime("%A, %B %d, %Y"))
-  end
-
-  it 'shows the invoice customers first and last name' do
-    expect(page).to have_content(@invoices[0].customer.first_name)
-    expect(page).to have_content(@invoices[0].customer.last_name)
+    end
   end
 
 end
