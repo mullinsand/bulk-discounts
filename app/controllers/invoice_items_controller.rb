974d01ad2ed17f60551666cb7c795fe1222cbcc2
Andrew Mullins <mullinsand15@gmail.com>
2022-09-19 16:14:13 -0600

    change invoice item variable method to find by

--- app/controllers/invoice_items_controller.rb
+++ app/controllers/invoice_items_controller.rb
@@ -1,6 +1,6 @@
 class InvoiceItemsController < ApplicationController
   def update
-    @invoice_item = InvoiceItem.where(item_id: params[:item_id], invoice_id: params[:invoice_id]).first
+    @invoice_item = InvoiceItem.find_by(item_id: params[:item_id], invoice_id: params[:invoice_id])
     if params[:status]
       @invoice_item.update(invoice_item_params)
       redirect_to merchant_invoice_path(params[:merchant_id], params[:invoice_id])
