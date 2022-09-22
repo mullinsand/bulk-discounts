70cfddc7b23de83fb6a67e618213eea6b854faf6
Andrew Mullins <mullinsand15@gmail.com>
2022-09-22 16:22:38 -0600

    bulk discounts index complete with passing tests

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -21,25 +21,27 @@ RSpec.describe 'bulk discounts Index' do
             expect(page).to have_content("#{bulk_discount.threshold}")
           end
         end
+        expect(page).to_not have_css("#discount_#{bulk_discount_5.id}")
       end
     end
 
-    xit 'has each merchant name is a link to the admin merchant show page' do
-      merch1 = Merchant.create!(name: Faker::Name.name)
-      merch2 = Merchant.create!(name: Faker::Name.name)
-      merch3 = Merchant.create!(name: Faker::Name.name)
-      merch4 = Merchant.create!(name: Faker::Name.name)
-      merch5 = Merchant.create!(name: Faker::Name.name)
+    it 'has each bulk discount is a link to the bulk discount show page' do
+      merch1 = create(:merchant)
+      bulk_discount_1 = create(:bulk_discount, merchant: merch1)
+      bulk_discount_2 = create(:bulk_discount, merchant: merch1)
+      bulk_discount_3 = create(:bulk_discount, merchant: merch1)
+      bulk_discount_4 = create(:bulk_discount, merchant: merch1)
+      bulk_discount_5 = create(:bulk_discount)
 
-      all_merchants = [merch1, merch2, merch3, merch4, merch5]
-      visit admin_merchants_path
+      all_bulk_discounts = [bulk_discount_1, bulk_discount_2, bulk_discount_3, bulk_discount_4]
+      visit merchant_bulk_discounts_path(merch1.id)
 
-      within '#merchants' do
-        all_merchants.each do |merchant|
-          within "#merchant_#{merchant.id}" do
-            click_link("#{merchant.name}")
-            expect(page).to have_current_path("/admin/merchants/#{merchant.id}")
-            visit admin_merchants_path
+      within '#discounts' do
+        all_bulk_discounts.each do |bulk_discount|
+          within "#discount_#{bulk_discount.id}" do
+            click_link("Bulk Discount ##{bulk_discount.id}")
+            expect(page).to have_current_path(merchant_bulk_discount_path(bulk_discount.merchant_id, bulk_discount))
+            visit merchant_bulk_discounts_path(merch1.id)
           end
         end
       end
