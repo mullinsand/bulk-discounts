fd702332cb72cfc36a3e09b9c3f53876e5311f52
Andrew Mullins <mullinsand15@gmail.com>
2022-09-22 19:53:45 -0600

    add bulk discount show page with test

---
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'bulk discounts show' do
+  describe "Show page has the discount and threshold for that bulk discount" do
+    it 'shows discount and threshold for that bulk discount' do
+      merch1 = create(:merchant)
+      bulk_discount_1 = create(:bulk_discount, merchant: merch1)
+      bulk_discount_2 = create(:bulk_discount, merchant: merch1)
+      bulk_discount_3 = create(:bulk_discount, merchant: merch1)
+      bulk_discount_4 = create(:bulk_discount, merchant: merch1)
+      bulk_discount_5 = create(:bulk_discount)
+
+
+      all_bulk_discounts = [bulk_discount_1, bulk_discount_2, bulk_discount_3, bulk_discount_4]
+      visit merchant_bulk_discount_path(merch1.id, bulk_discount_1)
+
+      within "#discount_#{bulk_discount_1.id}" do
+        expect(page).to have_content("#{bulk_discount_1.discount}")
+        expect(page).to have_content("#{bulk_discount_1.threshold}")
+      end
+      expect(page).to_not have_css("#discount_#{bulk_discount_5.id}")
+      expect(page).to_not have_css("#discount_#{bulk_discount_2.id}")
+    end
+  end
+end
